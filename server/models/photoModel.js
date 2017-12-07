f18a80361fde20f9e6ff7cececaa4ca152b753ef
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-12-07 14:25:47 +0700

    add photo schema

---
+++ server/models/photoModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const photoSchema = new Schema({
+  uploader: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  caption: String,
+  url: String,
+  like: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  uploadedAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: null
+  }
+});
+
+const Photo = mongoose.model('Photo', photoSchema);
+
+module.exports = Photo;
