522cb8b9b4a2fe8478b915795b866a27ab0a573f
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-12-07 15:07:17 +0700

    add navbar

---
+++ client/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <Navbar/>
+</template>
+
+<script>
+  import Navbar from '@/components/Navbar'
+  export default {
+    name: 'Home',
+    components: {
+      Navbar
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="ui attached stackable menu">
+    <div class="ui container">
+      <a class="item">
+        <i class="home icon"></i> Home
+      </a>
+      <a class="item">
+        <i class="grid layout icon"></i> Browse
+      </a>
+      <a class="item">
+        <i class="mail icon"></i> Messages
+      </a>
+      <div class="ui simple dropdown item">
+        More
+        <i class="dropdown icon"></i>
+        <div class="menu">
+          <a class="item"><i class="edit icon"></i> Edit Profile</a>
+          <a class="item"><i class="globe icon"></i> Choose Language</a>
+          <a class="item"><i class="settings icon"></i> Account Settings</a>
+        </div>
+      </div>
+      <div class="right item">
+        <button class="ui facebook button" @click="login">
+          <i class="facebook icon"></i>
+          Sign in with Facebook
+        </button>
+        <button class="ui facebook button">
+          <i class="sign out icon"></i>
+          Logout
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+    methods: {
+      login () {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
