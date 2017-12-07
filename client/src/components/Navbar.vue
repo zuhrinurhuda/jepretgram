7c9b49a25bf09a821e1bdb5f5f7931fad02d3a13
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-12-07 15:48:42 +0700

    add facebook login

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -20,13 +20,13 @@
         </div>
       </div>
       <div class="right item">
-        <button class="ui facebook button" @click="login">
-          <i class="facebook icon"></i>
-          Sign in with Facebook
-        </button>
-        <button class="ui facebook button">
+        <button class="ui facebook button" @click="logout" v-if="token">
           <i class="sign out icon"></i>
           Logout
+        </button>
+        <button class="ui facebook button" @click="login" v-else>
+          <i class="facebook icon"></i>
+          Sign in with Facebook
         </button>
       </div>
     </div>
@@ -36,22 +36,53 @@
 <script>
   export default {
     name: 'Navbar',
-    methods: {
-      login () {
-        FB.login(function(response) {
-          if (response.status === 'connected') {
-            FB.api('/me', function(response) {
-              console.log('Successful login for: ' + response.name);
-              
-            });
-          } else {
-
-          }
-        })
+    data: function () {
+      return {
+        token: localStorage.getItem('token')
       }
     },
-    created : {
-
+    methods: {
+      login () {
+        window.FB.login(response => {
+          console.log('fblogin response ', response)
+          if (response.status === 'connected') {
+            window.FB.api('/me', { fields: ['id', 'name', 'email'] }, data => {
+              this.$http.post('/users', data)
+              .then(result => {
+                console.log(result)
+                window.localStorage.setItem('token', result.data.token)
+                window.location.reload()
+              })
+              .catch(err => console.log(err))
+            })
+          } else {
+            console.log('User cancelled login or did not fully authorize.')
+          }
+        }, {scope: 'public_profile, email'})
+      },
+      logout () {
+        window.localStorage.removeItem('token')
+        window.location.reload()
+      }
+    },
+    created: () => {
+      window.fbAsyncInit = function () {
+        window.FB.init({
+          appId: '1826498564044116',
+          cookie: true,  // enable cookies to allow the server to access the session
+          xfbml: true,  // parse social plugins on this page
+          version: 'v2.8' // use graph api version 2.8
+        })
+      };
+      (function (d, s, id) { // Load the SDK asynchronously
+        var js
+        var fjs = d.getElementsByTagName(s)[0]
+        if (d.getElementById(id)) return
+        js = d.createElement(s)
+        js.id = id
+        js.src = 'https://connect.facebook.net/en_US/sdk.js'
+        fjs.parentNode.insertBefore(js, fjs)
+      }(document, 'script', 'facebook-jssdk'))
     }
   }
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import store from './vuex/index'
+
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:3000'
+})
 
 Vue.config.productionTip = false
 
@@ -10,6 +16,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
