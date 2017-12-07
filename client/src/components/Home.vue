eba4c3ed2f92c61b6f5c189f8f9bebc998f545a1
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-12-07 16:16:17 +0700

    add modal for submit photo

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -6,6 +6,7 @@
         <div class="row">
           <div class="column">
             <button class="ui right floated positive button" @click="addPhotoModal">Add Photo</button>
+            <Modal/>
           </div>
         </div>
       </header>
@@ -16,14 +17,16 @@
 
 <script>
   import Navbar from '@/components/Navbar'
+  import Modal from '@/components/Modal'
   export default {
     name: 'Home',
     components: {
-      Navbar
+      Navbar,
+      Modal
     },
     methods: {
       addPhotoModal () {
-        
+        $('#addPhoto').modal('show')
       }
     }
   }
---
+++ client/src/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="addPhoto" class="ui tiny modal">
+    <i class="close icon"></i>
+    <div class="header">
+      Add Photo
+    </div>
+    <div class="content">
+      <div class="ui form">
+        <div class="field">
+          <label>Url</label>
+          <input type="text" name="url" placeholder="url">
+        </div>
+        <div class="field">
+          <label>Caption</label>
+          <input type="text" name="caption" placeholder="caption">
+        </div>
+      </div>
+    </div>
+    <div class="actions">
+      <div class="ui black deny button">
+        Cancel
+      </div>
+      <div class="ui positive icon button">
+        Submit
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Modal'
+  }
+</script>
+
+<style lang="css">
+</style>
